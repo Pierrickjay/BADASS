f2a278bbcef9abe93ca5bacda261b43d020e9f58
jean-Michel Cervoni <jean.michel.cervoni@gmail.com>
2024-09-17 11:56:04 +0200

    P3 DONE Remove vty_conf files, content added in router's script; Add addresses on router to share a net address with hosts; Add a vtysh.conf sample to avoid "no such file" error on update.

--- P3/exec_config.sh
+++ P3/exec_config.sh
@@ -6,13 +6,14 @@ do
     if [[ $hostname =~ router ]]; then
         echo "Let's update container's ${hostname} configuration and exec it"
         docker cp ${hostname} $i:/tmp/
-        docker cp ${hostname}_vty_conf $i:/tmp
+        docker cp ./vtysh.conf.sample $i:/etc/frr/vtysh.conf
         docker exec $i chmod +x /tmp/${hostname}
         docker exec $i /tmp/${hostname}
         echo "end of configuration for container ${hostname}"
     else
         addr=${hostname: -1}
         docker exec $i ip addr add 20.1.1.${addr}/24 dev eth0
+        echo "address 20.1.1.${addr} added on eth0 for ${hostname}"
     fi
 
 done
